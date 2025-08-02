93575659f17ebf2aca6bcfc2133a7ec65c908649
kazuki-saito803 <kaz.saito803@gmail.com>
2025-08-02 13:56:25 +0900

    update step4

--- step5/main.py
+++ step5/main.py
@@ -1,12 +1,17 @@
+from flask import Flask, request, jsonify
 import os
-from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    message = os.environ.get("MESSAGE", "Default Message")
-    return message
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+@app.route("/")
+def home():
+    user = request.authorization
+    if user and user.username == USERNAME and user.password == PASSWORD:
+        return "Hello World with authentication", 200
+    return jsonify({"error": "Unauthorized"}), 401
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
